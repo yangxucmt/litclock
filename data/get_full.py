c2da38526af845a72dd9b2ea332b087f905b5806
Xu Yang <yangxusolidstate@gmail.com>
2024-10-19 22:41:30 +0800

    Fixed several formatting issues

--- data/get_full.py
+++ data/get_full.py
@@ -1,6 +1,7 @@
 import json
 import csv
 
+replacement_pair=[['<br>',' '],['<br/>',' '],['Mr','Mr.'],['Dr','Dr.'],['T S Eliot','T. S. Eliot'],['le Carré','Le Carré'],['Le Carre','Le Carré']]
 with open('white.csv','r') as csvfile:
     csvreader=csv.reader(csvfile)
     whitelist=[row for row in csvreader]
@@ -13,14 +14,17 @@ with open('litclock_annotated.csv', 'r') as csvfile:
   csv_reader = csv.reader(csvfile,delimiter='|')
   for row in csv_reader:
     indicator=False
+    row[3]=(row[3]).rstrip()
+    row[4]=(row[4]).rstrip() 
+    newrow=row
+    for cell in replacement_pair:
+        newrow=[newcell.replace(cell[0],cell[1]) for newcell in newrow]      
     for string in whitelist:
-        findbool=any(string in cell for cell in row)
+        findbool=any(string in cell for cell in newrow)
         if findbool:
             #print(f"Found '{string}' in row: {row}")
             indicator=True
     if indicator:
-        newrow=[cell.replace('Mr.','Mr') for cell in row]
-        newrow=[cell.replace('<br>',' ') for cell in newrow]
         string_records.append(newrow)
 
 with open('litclock_annotated_br2.csv', 'r') as csvfile:
@@ -29,16 +33,18 @@ with open('litclock_annotated_br2.csv', 'r') as csvfile:
   for row in csv_reader:
     indicator=False
     duplicate=True
+    row[3]=(row[3]).rstrip()
+    row[4]=(row[4]).rstrip()  
+    newrow=row
+    for cell in replacement_pair:
+        newrow=[newcell.replace(cell[0],cell[1]) for newcell in newrow]     
     for string in whitelist:
-        findbool=any(string in cell for cell in row)
+        findbool=any(string in cell for cell in newrow)
         if findbool:
             #print(f"Found '{string}' in row: {row}")
             indicator=True
-
     if indicator:
         for circlerow in string_records:
-            newrow=[cell.replace('Mr.','Mr') for cell in row]
-            newrow=[cell.replace('<br>',' ') for cell in newrow]
             if (circlerow[1]==newrow[1])&(circlerow[2]==newrow[2])&(circlerow[4]==newrow[4]):
                 duplicate=False
         if duplicate:
@@ -53,16 +59,16 @@ with open('Fx9.csv', 'r') as csvfile:
     duplicate=True
     row[3]=(row[3]).rstrip()
     row[4]=(row[4]).rstrip()
+    newrow=row
+    for cell in replacement_pair:
+        newrow=[newcell.replace(cell[0],cell[1]) for newcell in newrow]    
     for string in whitelist:
-        findbool=any(string in cell for cell in row)
+        findbool=any(string in cell for cell in newrow)
         if findbool:
             #print(f"Found '{string}' in row: {row}")
             indicator=True
-
     if indicator:
         for circlerow in string_records:
-            newrow=[cell.replace('Mr.','Mr') for cell in row]  
-            newrow=[cell.replace('<br>',' ') for cell in newrow]
             if (circlerow[1]==newrow[1])&(circlerow[2]==newrow[2])&(circlerow[4]==newrow[4]):
                 duplicate=False
         if duplicate:
@@ -75,17 +81,17 @@ with open('litclock_annotated_ver.csv', 'r') as csvfile:
     indicator=False
     duplicate=True
     row[3]=(row[3]).rstrip()
-    row[4]=(row[4]).rstrip()    
+    row[4]=(row[4]).rstrip() 
+    newrow=row
+    for cell in replacement_pair:
+        newrow=[newcell.replace(cell[0],cell[1]) for newcell in newrow]      
     for string in whitelist:
-        findbool=any(string in cell for cell in row)
+        findbool=any(string in cell for cell in newrow)
         if findbool:
             #print(f"Found '{string}' in row: {row}")
             indicator=True
-
     if indicator:
         for circlerow in string_records:
-            newrow=[cell.replace('Mr.','Mr') for cell in row]
-            newrow=[cell.replace('<br>',' ') for cell in newrow]
             if (circlerow[1]==newrow[1])&(circlerow[2]==newrow[2])&(circlerow[4]==newrow[4]):
                 duplicate=False
         if duplicate:
